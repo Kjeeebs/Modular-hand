0b18a20d8310e896cde485d4e781354ca12e7a08
Kjeeebs <95242589+Kjeeebs@users.noreply.github.com>
2021-12-01 16:12:24 +0100

    Update Styrehanske.ino

--- Styrehanske.ino
+++ Styrehanske.ino
@@ -1,5 +1,7 @@
 #include <SoftwareSerial.h>
-SoftwareSerial SUART(1,2);
+SoftwareSerial SUART(9,10); //Rx and tx
+
+
 
 int ResFinger1   = A0;  // Variables of the analog read form the flex sensors connected to the analog pins of Arduino LilyPad
 int ResFinger2   = A1;  
@@ -18,15 +20,19 @@ int OpenedFinger3   =0;
 
 int ClosedFinger1;      // Variables of the values when the hand is completely closed
 int ClosedFinger2;      // We can't set it to zero since that the minimum value reached
-int ClosedFinger3;     // in the analog read never reach zero. We'll assign the value of
+int ClosedFinger3;      // in the analog read never reach zero. We'll assign the value of
 // a first analog read, then the program in the loop will automatically  assing lower values
 
-int Finger1   =0;       // Variables of the values to send
-int Finger2   =0;
-int Finger3  =0;
+int Finger1_1   =0;       // Variables of the values to send
+int Finger2_1   =0;
+int Finger3_1   =0;
+int Finger1_2   =0;
+int Finger2_2   =0;
+int Finger3_2   =0;
 
-
-
+int Finger1 =0; //Res sensor values
+int Finger2 =0;
+int Finger3 =0;
 void setup()
 {
   Serial.begin(9600);   // Activating serial communication, using serialsoftware.h
@@ -36,9 +42,7 @@ void setup()
   pinMode(ResFinger2,   INPUT);
   pinMode(ResFinger3,   INPUT);
 
-  pinMode(VibFinger1, OUTPUT);
-  pinMode(VibFinger2, OUTPUT);
-  pinMode(VibFinger3, OUTPUT);
+  
 
   ClosedFinger1   = analogRead(ResFinger1);
   ClosedFinger2   = analogRead(ResFinger2);  
@@ -71,41 +75,22 @@ void loop()
   ClosedFinger3  = Finger3;
   
   
-  Finger1_1   = map(Finger1  ,ClosedFinger1  ,OpenedFinger1  ,0,180);  // The analog read has to be readapted in values between 0 and 180 to be used by the servomotors.
-  Finger2_1   = map(Finger2  ,ClosedFinger2  ,OpenedFinger2  ,0,180);  // The minimum and maximum values from the calibrations are used to correctly set the analog reads.
-  Finger3_1   = map(Finger3  ,ClosedFinger3  ,OpenedFinger3  ,0,180);
+  Finger1_1   = map(Finger1  ,ClosedFinger1  ,OpenedFinger1  ,10,170);  // The analog read has to be readapted in values between 0 and 180 to be used by the servomotors.
+  Finger2_1   = map(Finger2  ,ClosedFinger2  ,OpenedFinger2  ,10,170);  // The minimum and maximum values from the calibrations are used to correctly set the analog reads.
+  Finger3_1   = map(Finger3  ,ClosedFinger3  ,OpenedFinger3  ,10,170);
   Finger1_2   = map(Finger1  ,ClosedFinger1  ,OpenedFinger1  ,0,70);
   Finger2_2   = map(Finger2  ,ClosedFinger2  ,OpenedFinger2  ,0,70);
   Finger3_2   = map(Finger3  ,ClosedFinger3  ,OpenedFinger3  ,0,70);
   
   SUART.write("<");        // This character represent the beginning of the package of the three values
-  SUART.write(Finger1_1);    // The values are sent via the Tx pin (the digital pin 1)
+  SUART.write(Finger1_1);    // The values are sent via the Tx pin (the digital pin 10)
   SUART.write(Finger2_1);  
   SUART.write(Finger3_1); 
   SUART.write(Finger1_2);
   SUART.write(Finger2_2);
   SUART.write(Finger3_2);
   
-  delay(30);
-  
-  if(SUART.available()) {
-    
-    startPackage2 = SUART.read();
-    VibFingerS1 = SUART.read();
-    VibFingerS2 = SUART.read();
-    VibFingerS3 = SUART.read();
-    
-    if(startPackage2 == ">"){
-      
-      if(VibFingerS1 = HIGH)
-        VibFinger1.write(HIGH);
-      
-      if(VibFingerS2 = HIGH)
-        VibFinger2.write(HIGH);
-      
-      if(VibFingerS3 = HIGH)
-        VibFinger3.write = HIGH);
-      
-    }
+  delay(50);
+  Serial.println(Finger1_1);
   
 }
